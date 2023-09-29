d6399fe6fb21d38ab4d6ddc25edaa84904c0cb55
artemshurenkov <artemshurenkov@gmail.com>
2023-09-29 20:18:59 +0300

    Tasks 1-3 done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,36 @@
-import UIKit
+//Task1
+let numbers = 1...12
+for number in numbers {
+  switch number {
+    case 10: print("\(number) - true")
+    default: print("\(number) - false")
+  }
+}
 
-var greeting = "Hello, playground"
+//Task2
+var min = 0...59
+for moment in min {
+  if moment <= 15 {
+    print("\(moment) minute falls in the first part of the hour")
+  } else if moment >= 15 && moment <= 30 {
+    print("\(moment) minute falls in the second part of the hour")
+  } else if moment >= 30 && moment <= 45 {
+    print("\(moment) minute falls in the third part of the hour")
+  } else if moment >= 45 && moment <= 59 {
+    print("\(moment) minute falls in the fourth part of the hour")
+  }
+    
+}
+
+//Task3
+var lang = ["ru", "en"]
+var text = ""
+
+for selection in lang {
+  switch selection{
+    case "ru": text = "Понедельник"
+    case "en": text = "Monday"
+    default: print("Something went wrong")
+  }
+  print(selection, text)
+}
