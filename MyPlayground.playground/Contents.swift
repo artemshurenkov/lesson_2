a34713b82991e64d9d14e09bf0c478e935fed5a4
artemshurenkov <artemshurenkov@gmail.com>
2023-09-29 20:44:21 +0300

    Tasks 5-16 done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -38,9 +38,136 @@ for selection in lang {
 //Task 4
 //var a = 1
 //var a = 0
-var a = -3
+/*var a = -3
 if a == 0 {
   print("Верно")
 } else {
         print("Неверно")
+}*/
+
+//Task5
+//var a = 1
+//var a = 0
+/*var a = -3
+if a > 0 {
+  print("Верно")
+} else {
+        print("Неверно")
+}*/
+
+//Task 6
+//var a = 1
+//var a = 0
+/*var a = -3
+if a < 0 {
+  print("Верно")
+} else {
+        print("Неверно")
+}*/
+
+//Task 7
+//var a = 1
+//var a = 0
+/*var a = -3
+if a >= 0 {
+  print("Верно")
+} else {
+        print("Неверно")
+}*/
+
+//Task 8
+//var a = 1
+//var a = 0
+/*var a = -3
+if a <= 0 {
+  print("Верно")
+} else {
+        print("Неверно")
+}*/
+
+//Task 9
+//var a = 1
+//var a = 0
+/*var a = -3
+if a != 0 {
+  print("Верно")
+} else {
+        print("Неверно")
+}*/
+
+//Task 10
+//var test = "test"
+// var test = "тест"
+//var test = 3 //crash code
+/*if test == "test" {
+  print("Верно")
+} else {
+        print("Неверно")
+}*/
+
+//Task 11
+//var test = true
+/*var test = false
+
+if test == true {
+  print("Верно")
+} else {
+        print("Неверно")
+}*/
+
+//Task 12
+//var test = true
+var test = false
+
+if test != true {
+  print("Верно")
+} else {
+        print("Неверно")
+}
+
+//Task 13
+//var a = 5
+//var a = 0
+//var a = -3
+/*var a = 2
+if a >= 0 && a < 5 {
+  print("Верно")
+} else {
+  print("Неверно")
+}*/
+
+//Task 14
+//var a = 5
+//var a = 0
+//var a = -3
+/*var a = 2
+
+if a == 0 || a == 2 {
+  a += 7
+} else {
+  a /= 10
+}
+  print(a)*/
+
+/*Task 15
+//var a = 1
+var a = 3
+//var b = 6
+//var b = 3
+var b = 5
+
+if a <= 1 && b >= 3 {
+  print(a + b)
+} else {
+  print(a - b)
+}*/
+
+//Task 16
+var a = 3
+var b = 7
+
+if a > 2 && a < 11 && b >= 6 && b < 14 {
+  print("Верно")
+} else {
+  print("Неверно")
 }
