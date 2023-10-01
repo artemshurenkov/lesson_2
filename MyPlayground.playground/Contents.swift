cf722f5ed6cfb1bba477c99ea36913f451fb624a
artemshurenkov <artemshurenkov@gmail.com>
2023-10-02 01:49:55 +0300

    Correction tasks 20-24

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -301,4 +301,108 @@ var string: String?
         string = string! + ", " + element
         }
     }
-    print(string! + ". И это меньше 50. ")
+    print(string! + ". И это меньше 50.")
+
+//new 24
+var a1 = -188
+var b2 = String(a1)
+var symbols3 = Array(b2)
+var final = ""
+
+for c1 in symbols3{
+    switch c1 {
+    case "-": final += "минус "
+    case "0": final += "ноль, "
+    case "1": final += "один, "
+    case "2": final += "два, "
+    case "3": final += "три, "
+    case "4": final += "четыре, "
+    case "5": final += "пять, "
+    case "6": final += "шесть, "
+    case "7": final += "семь, "
+    case "8": final += "восемь, "
+    case "9": final += "девять, "
+    default:break
+    }
+}
+print("\(final)и это меньше 50.")
+
+//new 23
+
+var a2 = "123222"
+var symbols4 = Array(a2)
+var sum11 = Int()
+var sum22 = Int()
+for c2 in symbols4[0...2]{
+    switch c2{
+    case "1": sum11 += 1
+    case "2": sum11 += 2
+    case "3": sum11 += 3
+    case "4": sum11 += 4
+    case "5": sum11 += 5
+    case "6": sum11 += 6
+    case "7": sum11 += 7
+    case "8": sum11 += 8
+    case "9": sum11 += 9
+    default:break
+    }
+}
+for c3 in symbols4[3...5]{
+    switch c3{
+    case "1": sum22 += 1
+    case "2": sum22 += 2
+    case "3": sum22 += 3
+    case "4": sum22 += 4
+    case "5": sum22 += 5
+    case "6": sum22 += 6
+    case "7": sum22 += 7
+    case "8": sum22 += 8
+    case "9": sum22 += 9
+    default:break
+    }
+}
+
+if sum11 == sum22{
+    print ("da")
+} else {
+    print("net")
+}
+
+//new 22
+var a3 = "969"
+var symbols5 = Array(a3)
+var sum33 = Int()
+for c4 in symbols5{
+    switch c4{
+    case "1": sum33 += 1
+    case "2": sum33 += 2
+    case "3": sum33 += 3
+    case "4": sum33 += 4
+    case "5": sum33 += 5
+    case "6": sum33 += 6
+    case "7": sum33 += 7
+    case "8": sum33 += 8
+    case "9": sum33 += 9
+    default:break
+    }
+}
+print(sum33)
+
+//new 21
+var a4 = "12345"
+var b3 = Array(a4)
+if b3[0] == "1" {
+    print("da")
+} else {
+    print("net")
+}
+
+//new 20
+
+var a5 = "GachiMuchi"
+var b4 = Array(a5)
+if b4[0] == "a" {
+    print("da")
+} else {
+    print("net")
+}
